ba8f7dbb4d5aca3388f7b55e6baf8ffe81bed590
me <me>
2026-03-23 16:40:11 +0530

    This is java file

---
+++ Java.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Java program executed successfully!");
+        System.out.println("Directory and file operations done through Jenkins pipeline.");
+    }
+}
