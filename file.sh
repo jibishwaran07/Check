539629d89bd5d9d05861d8097a3772f81e68d4e1
me <me>
2026-03-23 15:15:19 +0530

    Check changes

---
+++ file.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+TARGET_DIR="/home/administrator/myfolder"
+TARGET_FILE="$TARGET_DIR/myfile.txt"
+
+sudo mkdir -p "$TARGET_DIR"
+sudo touch "$TARGET_FILE"
+
+sudo chmod 755 "$TARGET_DIR"
+sudo chmod 644 "$TARGET_FILE"
+
+sudo chown administrator:administrator "$TARGET_DIR" "$TARGET_FILE"
+
+echo "Jenkins job completed!"
